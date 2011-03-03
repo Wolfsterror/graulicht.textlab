de9c8064c45bfdf9d08af5f59706d29811f1b6cb
Raphael Pohl <fuchs@eisfuchslabor.de>
2011-03-03 12:07:42 +0100

    FlashDevelop overwrites config files.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mxmlc -library-path+=/opt/flex/frameworks/libs/ -source-path+=/opt/flex/frameworks/projects/framework/src/ -load-config /opt/flex/frameworks/flex-config.xml -load-config ./obj/textlabConfig.xml -library-path+=./src/com/ -source-path+=./src/ -output ./bin/textlab.swf -debug=false -incremental=true -benchmark=false -static-link-runtime-shared-libraries=true -optimize=true
+mxmlc -library-path+=/opt/flex/frameworks/libs/ -source-path+=/opt/flex/frameworks/projects/framework/src/ -load-config /opt/flex/frameworks/flex-config.xml -load-config ./obj/textlabLinux.xml -library-path+=./src/com/ -source-path+=./src/ -output ./bin/textlab.swf -debug=false -incremental=true -benchmark=false -static-link-runtime-shared-libraries=true -optimize=true
 
 # fdb bin/textlab.swf
